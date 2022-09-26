b74161530ec3b213fe99a4d60743e2afb7d3da72
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-26 20:10:58 +0300

    Added Pages Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route } from "react-router-dom";
+import { Fragment } from "react";
+import Welcome from "./pages/Welcome";
+import Products from './pages/Products';
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products/>
+      </Route>
+    </Fragment>
   );
 }
 
---
+++ src/pages/Welcome.js
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Welcome to this Page</h2>
+    </div>
+  );
+};
+
+export default Welcome;
