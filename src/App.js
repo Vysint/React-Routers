28262480dbed034ea26388a59f000bd694333d97
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-26 21:03:50 +0300

    Extracted Routes Params

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Route path="/products">
         <Products/>
       </Route>
-      <Route path="/product-detail/:productid">
+      <Route path="/product-detail/:productId">
         <ProductDetail/>
       </Route>
       </main>
